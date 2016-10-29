d4638defc4a436fb44c443357cb548b2edaed20c
Fetiu <k9380450@gmail.com>
2016-10-29 16:54:44 +0000

    activating confirmed

---
+++ app/run.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, make_response, request
+import random
+app =Flask(__name__)
+
+color_list=['RED','MAROON','BLUE','AQUA','GREEN','LIME','PURPLE','FUCHSIA']
+
+@app.route('/visitcounter')
+def index():
+	if 'cnt' in request.cookies:
+		count = int(request.cookies.get('cnt'))
+	else: count = 0
+
+	resp= make_response(render_template
+							('index.html',
+							size=str(random.randrange(5,10)),
+							color=random.choice(color_list),
+							number=str(count))
+							)
+	count+=1
+	resp.set_cookie('cnt',str(count))
+	return resp
+
+if __name__=='__main__':
+	app.run(host='0.0.0.0', port=5050, debug=True)
+
